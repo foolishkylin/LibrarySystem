94d213f1009ca8804abc3c6318c9389448ddffae
foolishkylin <foolishkylin@gmail.com>
2019-12-06 10:49:22 +0800

    update the main function

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 	BTree t = (BTree)malloc(sizeof(BTNode));
 	if (t && res)
 	{
-		int test[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int test[10] = { 60, 20, 30, 70, 72, 50, 52, 68, 71, 100 };
 		t->parent = NULL;
 		for (int i = 0; i <= M; i++)
 			t->ptr[i] = NULL;
@@ -15,11 +15,17 @@ int main()
 		for (int i = 0; i < 10; i++)
 		{
 			SearchBTree(t, test[i], res);
-			if (!res->tag)
-				InsertBTree(t, test[i], res->pt, res->i);
-			printf("\n--the %dth test, insert %d--\n\n", i+1, test[i]);
-			ShowBTree(t, 0);
+			if (res->tag == SEARCH_FAIL)
+				InsertBTree(t, test[i], res->ptr, res->pos);
+			if (i % 1 == 0)
+			{
+				printf("\n--the %dth test, insert %d--\n\n", i + 1, test[i]);
+				ShowBTree(t, 0);
+			}
 		}
+		SearchBTree(t, 50, res);
+		DeleteBTree(res->ptr, res->pos, t);
+		ShowBTree(t, 0);
 
 	}
 
