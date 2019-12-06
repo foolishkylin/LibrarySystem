0ba0979c4395142fb3cccd7dd9e84a6e7c2517f6
foolishkylin <foolishkylin@gmail.com>
2019-12-06 10:48:58 +0800

    update the the deleted function

--- b_tree.cpp
+++ b_tree.cpp
@@ -1,174 +1,406 @@
 #include "b_tree.h"
 
-int Search(BTree p, int k)
+int Search(BTree search_node, int search_value)
 // fun: searching in a no-left node 
-// ret: the postion of node which value <= k
+// ret: the postion of node which value <= search_value
 // key: none
 {
     int i = 1;
-    while(i <= p->keynum && k > p->key[i]) i++;
+    while(i <= search_node->keynum && search_value > search_node->key[i]) i++;
     return i;
 }
 
 
-void SearchBTree(BTree t, int k, RES &result)
+void SearchBTree(BTree tree, int search_value, RES &search_result)
 // fun: searching in a btree 
 // ret: a result ptr including (pt, i, tag)
 // key: if searching successfully, tag = 1, [*pt][i]=k
 //      else tag = 0, return the postion which can be inserted.
 {
-    int i = 0, found = 0;
-    BTree p = t, q = NULL;
+    int search_postion = 0, position_found = 0;
+    BTree current_node = tree, parent_node = NULL;
     // p points to node needed to being searched and q points to its parent
-    while(p && !found)
+    while(current_node && !position_found)
     {
-        i = Search(p, k);
-        if (i <= p->keynum && p->key[i] == k)
-            found = 1;
+        search_postion = Search(current_node, search_value);
+        if (search_postion <= current_node->keynum && current_node->key[search_postion] == search_value)
+            position_found = 1;
         else
         {
-            q = p;
-            p = p->ptr[i-1];
+            parent_node = current_node;
+            current_node = current_node->ptr[search_postion-1];
         }
     }
 
-    if (found)
+    if (position_found)
     {
-        result->pt = p;
-        result->i = i;
-        result->tag = 1;
+        search_result->ptr = current_node;
+        search_result->pos = search_postion;
+        search_result->tag = SEARCH_SUCCESS;
     }
     else
     {
-        result->pt = q;
-        result->i = i;
-        result->tag = 0;
+        search_result->ptr = parent_node;
+        search_result->pos = search_postion;
+        search_result->tag = SEARCH_FAIL;
     }
 }
 
 
-void split(BTree &q, int s, BTree &ap)
+void Split(BTree& current_node, int split_postion, BTree &splited_node)
+// fun: split the current_node
+// ret: none
+// key: for the position split_postio, cut the current_node->key, the front ones are belong to
+//		current_node and the behind are belong to splited_node 
 {
-	int i, j, n = q->keynum;
-	ap = (BTree)malloc(sizeof(BTNode));
-	if (ap)
+	int i, j, current_keynum = current_node->keynum;
+	splited_node = (BTree)malloc(sizeof(BTNode));
+	if (splited_node)
 	{
-		ap->ptr[0] = q->ptr[s];
-		for (i = s + 1, j = 1; i <= n; i++, j++)
+		// !!!!!!!!!!!!!!!!!!!!!!!!
+		for (int i = 0; i <= M; i++)
+			splited_node->ptr[i] = NULL;
+		// !!!!!!!!!!!!!!!!!!!!!!!!
+
+		splited_node->ptr[0] = current_node->ptr[split_postion];
+		for (i = split_postion + 1, j = 1; i <= current_keynum; i++, j++)
 		{
-			ap->key[j] = q->key[i];
-			ap->ptr[j] = q->ptr[i];
+			splited_node->key[j] = current_node->key[i];
+			splited_node->ptr[j] = current_node->ptr[i];
 		}
-		ap->keynum = n - s;
-		ap->parent = q->parent;
-		for (i = 0; i <= n - s; i++)
-			if (ap->ptr[i])
-				ap->ptr[i]->parent = ap;
-		q->keynum = s - 1;
+		splited_node->keynum = current_keynum - split_postion;
+		splited_node->parent = current_node->parent;
+		for (i = 0; i <= current_keynum - split_postion; i++)
+			if (splited_node->ptr[i])
+				splited_node->ptr[i]->parent = splited_node;
+		current_node->keynum = split_postion - 1;
 	}
 	else
 		printf("error in split: memory insufficient.");
 }
 
 
-void newRoot(BTree &t, BTree p, int x, BTree ap)
+void NewRoot(BTree& new_root, BTree left_son, int root_key, BTree right_son)
+// fun: create a new root for the nodes left_son and right_son
+// ret: none
+// key: if sons == 0, then this func is to create a true root
 {
-	t = (BTree)malloc(sizeof(BTNode));
-	if (t)
+	new_root = (BTree)malloc(sizeof(BTNode));
+	if (new_root)
 	{
-		t->keynum = 1;
-		t->ptr[0] = p;
-		t->ptr[1] = ap;
-		t->key[1] = x;
-		if (p)
-			p->parent = t;
-		if (ap)
-			ap->parent = t;
-		t->parent = NULL;
+		// !!!!!!!!!!!!!!!!!!!!!!!!
+		for (int i = 0; i <= M; i++)
+			new_root->ptr[i] = NULL;
+		// !!!!!!!!!!!!!!!!!!!!!!!!
+
+		new_root->keynum = 1;
+		new_root->ptr[0] = left_son;
+		new_root->ptr[1] = right_son;
+		new_root->key[1] = root_key;
+		if (left_son)
+			left_son->parent = new_root;
+		if (right_son)
+			right_son->parent = new_root;
+		new_root->parent = NULL;
 	}
 	else
 		printf("error in newRoot: memory insufficient.");
 }
 
 
-void Insert(BTree &q, int i, int x, BTree ap)
+void Insert(BTree& current_node, int inserted_postion, int inserted_value, BTree inserted_node)
+// fun: insert a new node
+// ret: none
+// key: none
 {
-	int j, n = q->keynum;
-	for (j = n; j >= i; j--)
+	int j, current_keynum = current_node->keynum;
+	for (j = current_keynum; j >= inserted_postion; j--)
 	{
-		q->key[j + 1] = q->key[j];
-		q->ptr[j + 1] = q->ptr[j];
+		current_node->key[j + 1] = current_node->key[j];
+		current_node->ptr[j + 1] = current_node->ptr[j];
 	}
-	q->key[i] = x;
-	q->ptr[i] = ap;
-	if (ap)
-		ap->parent = q;
-	q->keynum++;
+	current_node->key[inserted_postion] = inserted_value;
+	current_node->ptr[inserted_postion] = inserted_node;
+	if (inserted_node)
+		inserted_node->parent = current_node;
+	current_node->keynum++;
 }
 
 
-void InsertBTree(BTree &t, int k, BTree q, int i)
+void InsertBTree(BTree& tree, int inserted_value, BTree inserted_node, int insert_position)
+// fun: insert a new key to tree
+// ret: none
+// key: none
 {
-	int x, s, finished = 0, needNewRoot = 0;
-	BTree ap;
-	if (!q)
-		newRoot(t, NULL, k, NULL);
+	int current_inserted_value, splited_position, finished = FALSE, need_new_root = FALSE;
+	BTree splited_node;
+	if (!inserted_node)
+		// if the tree is empty
+		// create a root node
+		NewRoot(tree, NULL, inserted_value, NULL);
 	else
 	{
-		x = k, ap = NULL;
-		while (!needNewRoot && !finished)
+		current_inserted_value = inserted_value, splited_node = NULL;
+		while (!need_new_root && !finished)
 		{
-			Insert(q, i, x, ap);
-			if (q->keynum < M)
+			Insert(inserted_node, insert_position, current_inserted_value, splited_node);
+			if (inserted_node->keynum < M)
 				finished = 1;
 			else
+			// the node is full
 			{
-				s = (M + 1) / 2;
-				split(q, s, ap);
-				x = q->key[s];
-				if (q->parent)
+				splited_position = (M + 1) / 2;
+				Split(inserted_node, splited_position, splited_node);
+				current_inserted_value = inserted_node->key[splited_position];
+				// the current_inserted_value now will change, 
+				// maybe not equal to the inserted_value
+				if (inserted_node->parent)
 				{
-					q = q->parent;
-					i = Search(q, x);
+					inserted_node = inserted_node->parent;
+					insert_position = Search(inserted_node, current_inserted_value);
+					// find the position of key 
+					// which needed to insert to the parent
 				}
 				else
-					needNewRoot = 1;
+				// the tree is only a root node
+					need_new_root = TRUE;
 			}
 		}
-		if (needNewRoot)
-			newRoot(t, q, x, ap);
+		if (need_new_root)
+			NewRoot(tree, inserted_node, current_inserted_value, splited_node);
+			// create a new node for the inserted node and splited node
 	}
 
 }
 
-void DeleteBTree(BTree &p, int i)
+void DeleteBTree(BTree& deleted_node, int deleted_postion, BTree &t)
 {
-	//if (p->ptr[i])
-	//{
-	//	successor(p, i);
-	//	deletebtree(p, 1);
-	//}
-	//else
-	//{
-	//	remove(p, i);
-	//	if (p->keynum < (m - 1) / 2)
-	//		restore(p, i);
-	//}
+	if (deleted_node->ptr[deleted_postion])
+	{
+		Successor(deleted_node, deleted_postion);
+		DeleteBTree(deleted_node, 1, t);
+	}
+	else
+	{
+		Remove(deleted_node, deleted_postion);
+		printf("now the deleted node was removed\n");
+		printf("deleted_node->keynum (%d) : (M - 1) / 2 (%d)\n", deleted_node->keynum, (M - 1) / 2);
+		if (deleted_node->keynum < (M - 1) / 2)
+			Restore(deleted_node, deleted_postion, t);
+	}
+}
+
+void Remove(BTree& deleted_node, int deleted_postion)
+{
+	for (int i = deleted_postion; i < deleted_node->keynum; i++)
+	{
+		deleted_node->key[i] = deleted_node->key[i + 1];
+		deleted_node->ptr[i] = deleted_node->ptr[i + 1];
+	}
+	// deleted_node->ptr[deleted_node->keynum - 1] = deleted_node->ptr[deleted_node->keynum];
+	deleted_node->keynum -= 1;
 }
 
 
+void Successor(BTree& deleted_node, int deleted_postion)
+{
+
+}
+
+
+void Restore(BTree& deleted_node, int deleted_postion, BTree &t)
+{
+	if (!deleted_node->parent && !deleted_node->keynum)
+	{
+		t = deleted_node->ptr[0];
+		deleted_node->ptr[0]->parent = NULL;
+		free(deleted_node);
+		printf("the root was updated!\n\n");
+		return;
+	}
+	else if (!deleted_node->parent)
+		return ;
+	printf("--now should restore--\n\n");
+	BTree parent = deleted_node->parent, left_brother = NULL, right_brother = NULL;
+	KeyType parent_key = 0, parent_postion = 1;
+	int deleted_node_position = 0, left_found = FALSE, right_found = FALSE, parent_found = FALSE, i;
+	for (; deleted_node_position <= parent->keynum && parent->ptr[deleted_node_position] != deleted_node;
+		deleted_node_position++);
+	printf("deleted postion: %d\n\n", deleted_node_position);
+	if (deleted_node->keynum + 1 == (M - 1) / 2)
+	{
+		if (parent->ptr[deleted_node_position] != deleted_node)
+			printf("error in Restore: cannot find the deleted_node_position!\n");
+		if (deleted_node_position)
+		{
+			left_brother = parent->ptr[deleted_node_position - 1];
+			left_found = TRUE;
+		}
+		if (deleted_node_position < parent->keynum)
+		{
+			right_brother = parent->ptr[deleted_node_position + 1];
+			right_found = TRUE;
+		}
+		if (left_found && left_brother->keynum >= (M + 1) / 2)
+		{
+			printf("--now should adjust the left part--\n\n");
+			KeyType max_brother_key = left_brother->key[left_brother->keynum];
+			for (i = 1; i <= parent->keynum && ! parent_found; i++)
+			{
+				if (parent->key[i] > max_brother_key)
+				{
+					parent_key = parent->key[i];
+					printf("left rich: parent node than bigger than left-max-node was find\n\n");
+					parent_postion = i;
+					parent_found = TRUE;
+				}
+			}
+
+			// 为保证思路连贯，此处用中文写出
+			// 在deleted_node处需要向left_brother借key来调整的时候，需要注意以下几点：
+			// 1. 流程大致是先借来left_brother的key，再借来parent的key，把parent的key插入被删结点后，
+			//    再把left_brother的key替换掉parent那个借来的key，最后把left_brother的key删掉
+			// 2. 借来的left_brother的key必须是其最大的key，即left_brother->key[left_brother->keynum]
+			// 3. 借来的parent的key必须是恰好比借来的left_brother的key大的那一个key
+			// 4. parent的key必须插入在deleted_node中最小的位置，即1
+			// 5. 调整后需让兄弟的最大子过继给被调整结点
+			// 在向right_brother借key的时候，与如上相反。
+
+			deleted_node->keynum += 1;
+			// because the parent key is smaller than the deleted key
+			// so the key should be the min position of deleted node
+			for (i = deleted_node->keynum; i > 1; i--)
+				deleted_node->key[i] = deleted_node->key[i - 1];
+			deleted_node->key[1] = parent_key;
+			parent->key[parent_postion] = max_brother_key;
+
+			for (i = deleted_node->keynum; i > 0; i--)
+				deleted_node->ptr[i] = deleted_node->ptr[i - 1];
+			deleted_node->ptr[0] = left_brother->ptr[left_brother->keynum];
+			Remove(left_brother, left_brother->keynum);
+			// here the left->brother->ptr[left_brother->keynum] != NULL
+			// but the left->brother->keynum--. so it is ok.
+		}
+		else if (right_found && right_brother->keynum >= (M + 1) / 2)
+		{
+			printf("--now should adjust the right part--\n\n");
+			KeyType min_brother_key = right_brother->key[1];
+			for (i = parent->keynum; i && !parent_found; i--)
+				if (parent->key[i] < min_brother_key)
+				{
+					parent_key = parent->key[i];
+					printf("right rich: parent node than smaller than right-min-node was found\n\n");
+					parent_postion = i;
+					parent_found = TRUE;
+				}
+			deleted_node->keynum += 1;
+			// because the parent key is bigger than the deleted key
+			// so the key should be the max position of deleted node
+			deleted_node->key[deleted_node->keynum] = parent_key;
+			parent->key[parent_postion] = min_brother_key;
+
+			deleted_node->ptr[deleted_node->keynum] = right_brother->ptr[0];
+
+			for (int i = 1; i < right_brother->keynum; i++)
+			{
+				right_brother->key[i] = right_brother->key[i + 1];
+				right_brother->ptr[i-1] = right_brother->ptr[i];
+				// here need to delete right_brother->ptr[0]
+			}
+			right_brother->ptr[right_brother->keynum - 1] = right_brother->ptr[right_brother->keynum];
+			right_brother->keynum -= 1;
+		}
+		else
+		{
+			BTree parent = deleted_node->parent;
+			KeyType parent_key = 0;
+			printf("now merge\n\n");
+			if (left_found)
+			{
+				printf("now merge the left node\n\n");
+				// 并非(parent->keynum + 1) / 2]， 应是分割left_brother与deleted_node的位置
+				parent_key = parent->key[deleted_node_position];
+				// parent->ptr[deleted_node_position] = NULL;
+				Remove(parent, deleted_node_position);
+				// extract the middle key of parent
+				
+				left_brother->keynum += 1;
+				left_brother->key[left_brother->keynum] = parent_key;
+
+				while (deleted_node->keynum)
+				{
+					// left_brother->keynum++;
+					if (left_brother->keynum > 4)
+					{
+						printf("error in merge: node->keynum overflow!");
+						break;
+					}
+					left_brother->key[++left_brother->keynum] = deleted_node->key[1];
+					Remove(deleted_node, 1);
+				}
+				printf("parent_key_num: %d\n", parent->keynum);
+				free(deleted_node);
+			}
+			else
+			{
+				if (!right_found)
+				{
+					printf("error in merge: the left/right nodes were not found!\n\n");
+					return ;
+				}
+				printf("now merge the right node\n\n");
+				// 并非(parent->keynum + 1) / 2]， 应是分割left_brother与deleted_node的位置
+				parent_key = parent->key[deleted_node_position + 1];
+				// parent->ptr[deleted_node_position] = NULL;
+				Remove(parent, deleted_node_position+1);
+				// extract the middle key of parent
+
+				right_brother->keynum += 1;
+				for (i = right_brother->keynum; i > 1; i--)
+					right_brother->key[i] = right_brother->key[i - 1];
+				right_brother->key[1] = parent_key;
+
+				while (deleted_node->keynum)
+				{
+					// left_brother->keynum++;
+					for (i = right_brother->keynum; i > 1; i--)
+						right_brother->key[i] = right_brother->key[i - 1];
+					right_brother->key[1] = deleted_node->key[deleted_node->keynum];
+					Remove(deleted_node, deleted_node->keynum);
+				}
+				free(deleted_node);
+			}
+		}
+	}
+	if (parent->keynum < (M + 1) / 2)
+	{
+		printf("parent node is not required!\n\n");
+		// ShowBTree(t, 0);
+		Restore(parent, 1, t);
+	}
+}
+
 void ShowBTree(BTree root, int layer)
+// fun: show the whole btree
+// ret: none
+// key: print the tree according the layer of tree
 {
 	if (root)
 	{
-		ShowBTree(root->ptr[0], layer+1);
+		for (int j = 0; j < layer; j++)
+			printf("	  ");
 		for (int i = 1; i <= root->keynum; i++)
 		{	
-			for (int j = 0; j < layer; j++)
-				printf("    ");
-			printf("    %d\n", root->key[i]);
-			ShowBTree(root->ptr[i], layer + 1);
+			printf("%d", root->key[i]);
+			if (i != root->keynum)
+				printf(",");
+			else
+				printf("\n");
 		}
+		for (int i = 0; i <= root->keynum; i++)
+			ShowBTree(root->ptr[i], layer + 1);
+		//if (root->ptr[0] != null)
+		//	showbtree(root->ptr[0], layer + 1);
 	}
 }
 
--- b_tree.h
+++ b_tree.h
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define M 3
+#define FALSE 0
+#define TRUE 1
 typedef int KeyType;
 
 typedef struct BTNode
@@ -17,39 +19,66 @@ typedef struct BTNode
 
 typedef struct
 {
-	BTree pt;
-	int i;
+	BTree ptr;
+	int pos;
 	int tag;
 } RESNode, *RES;
 
-int Search(BTree p, int k);
+enum RESULT_TAG {
+	SEARCH_SUCCESS = 0, 
+	SEARCH_FAIL = 1
+};
+
+
+int Search(BTree search_node, int search_value);
 // fun: searching in a no-left node 
-// ret: the postion of node which value <= k
+// ret: the postion of node which value <= search_value
 // key: none
 
 
-void SearchBTree(BTree t, int k, RES &result);
+void SearchBTree(BTree tree, int search_value, RES& search_result);
 // fun: searching in a btree 
 // ret: a result ptr including (pt, i, tag)
 // key: if searching successfully, tag = 1, [*pt][i]=k
 //      else tag = 0, return the postion which can be inserted.
 
 
-void split(BTree &q, int s, BTree &ap);
+void Split(BTree& current_node, int split_postion, BTree& splited_node);
+// fun: split the current_node
+// ret: none
+// key: for the position split_postio, cut the current_node->key, the front ones are belong to
+//		current_node and the behind are belong to splited_node 
 
 
-void newRoot(BTree &t, BTree p, int x, BTree ap);
+void NewRoot(BTree& new_root, BTree left_son, int root_key, BTree right_son);
+// fun: create a new root for the nodes left_son and right_son
+// ret: none
+// key: if sons == 0, then this func is to create a true root
 
 
-void Insert(BTree &q, int i, int x, BTree ap);
+void Insert(BTree& current_node, int inserted_postion, int inserted_value, BTree inserted_node);
+// fun: insert a new node
+// ret: none
+// key: none
 
 
-void InsertBTree(BTree &t, int k, BTree q, int i);
+void InsertBTree(BTree& tree, int inserted_value, BTree inserted_node, int insert_position);
+// fun: insert a new key to tree
+// ret: none
+// key: none
 
 
-void DeleteBTree(BTree &p, int i);
+void DeleteBTree(BTree& deleted_node, int deleted_postion, BTree &t);
+
+void Successor(BTree& deleted_node, int deleted_postion);
+
+void Remove(BTree& deleted_node, int deleted_postion);
+
+void Restore(BTree& deleted_node, int deleted_postion, BTree &t);
 
 
 void ShowBTree(BTree root, int layer);
-//
+// fun: show the whole btree
+// ret: none
+// key: print the tree according the layer of tree
 #endif
