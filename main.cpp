3902819283117c56700ed994b8045ee6c18fda45
foolishkylin <foolishkylin@gmail.com>
2019-12-06 21:42:20 +0800

    updated the test data.

--- main.cpp
+++ main.cpp
@@ -46,3 +46,47 @@ int main()
 
 	return 0;
 }
+
+int test()
+{
+	RES res = (RES)malloc(sizeof(RESNode));
+	BTree t = (BTree)malloc(sizeof(BTNode));
+	if (t && res)
+	{
+		int test[18] = { 35, 16, 18, 70, 5, 50, 22, 60, 13, 17, 12 , 45, 25, 42, 15, 90, 30, 7 };
+		t->parent = NULL;
+		for (int i = 0; i <= M; i++)
+			t->ptr[i] = NULL;
+		t->keynum = 0;
+		for (int i = 0; i < 18; i++)
+		{
+			SearchBTree(t, test[i], res);
+			if (res->tag == SEARCH_FAIL)
+				InsertBTree(t, test[i], res->ptr, res->pos);
+			if (i % 1 == 0)
+			{
+				printf("\n--the %dth test, insert %d--\n\n", i + 1, test[i]);
+				ShowBTree(t, 0);
+			}
+		}
+		SearchBTree(t, 45, res);
+		DeleteBTree(res->ptr, res->pos, t);
+		ShowBTree(t, 0);
+		SearchBTree(t, 90, res);
+		DeleteBTree(res->ptr, res->pos, t);
+		ShowBTree(t, 0);
+		SearchBTree(t, 50, res);
+		DeleteBTree(res->ptr, res->pos, t);
+		ShowBTree(t, 0);
+		SearchBTree(t, 22, res);
+		DeleteBTree(res->ptr, res->pos, t);
+		ShowBTree(t, 0);
+		SearchBTree(t, 42, res);
+		DeleteBTree(res->ptr, res->pos, t);
+		ShowBTree(t, 0);
+
+	}
+
+
+	return 0;
+}
