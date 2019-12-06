e4166db4ac9b9baf72a74bfd37c216187ddbdd2b
foolishkylin <foolishkylin@gmail.com>
2019-12-06 17:42:37 +0800

    updated the test data.

--- main.cpp
+++ main.cpp
@@ -7,12 +7,12 @@ int main()
 	BTree t = (BTree)malloc(sizeof(BTNode));
 	if (t && res)
 	{
-		int test[10] = { 60, 20, 30, 70, 72, 50, 52, 68, 71, 100 };
+		int test[18] = { 35, 16, 18, 70, 5, 50, 22, 60, 13, 17, 12 , 45, 25, 42, 15, 90, 30, 7 };
 		t->parent = NULL;
 		for (int i = 0; i <= M; i++)
 			t->ptr[i] = NULL;
 		t->keynum = 0;
-		for (int i = 0; i < 10; i++)
+		for (int i = 0; i < 18; i++)
 		{
 			SearchBTree(t, test[i], res);
 			if (res->tag == SEARCH_FAIL)
@@ -23,7 +23,21 @@ int main()
 				ShowBTree(t, 0);
 			}
 		}
+		SearchBTree(t, 45, res);
+		DeleteBTree(res->ptr, res->pos, t);
+		ShowBTree(t, 0);
+		SearchBTree(t, 90, res);
+		DeleteBTree(res->ptr, res->pos, t);
+		ShowBTree(t, 0);
+		// 这里的42结点的父关系错误
+		// 42过继到50下面的时候没用改变其父属性
 		SearchBTree(t, 50, res);
+		DeleteBTree(res->ptr, res->pos, t);
+		ShowBTree(t, 0);
+		SearchBTree(t, 22, res);
+		DeleteBTree(res->ptr, res->pos, t);
+		ShowBTree(t, 0);
+		SearchBTree(t, 42, res);
 		DeleteBTree(res->ptr, res->pos, t);
 		ShowBTree(t, 0);
 
